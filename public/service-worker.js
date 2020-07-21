14aa2e9a7dc091284adca9e3b2a05236a463e82f
Armenuhi <avaarm95@gmail.com>
2020-07-20 19:34:52 -0700

    slashes taken out from service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 
-const FILES_TO_CACHE = ["/", "/index.html","/index.js","db.js", "icons/icon-192x192.png","icons/icon-512x512.png",'/manifest.webmanifest',
-'/styles.css']
+const FILES_TO_CACHE = ["/", "index.html","index.js","db.js", "icons/icon-192x192.png","icons/icon-512x512.png",'manifest.webmanifest',
+'styles.css']
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
